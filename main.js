300632eae30a801ca61e484ebe962fe3cb137b19
Daniil <34283275+Romashka19@users.noreply.github.com>
2020-02-03 03:00:16 -1000

    git push

--- main.js
+++ main.js
@@ -1,49 +1,19 @@
-let btn= document.getElementById('testBtn');
+let btn = document.getElementById('testBtn');
 
-btn.onclick = up;
-function up(event) {
-    btn.style.position = 'absolute';
-    btn.style.zIndex = 1000;
-    moveAt(event);
-    document.onmousemove = move;
-    btn.onclick = down;
+let check = true;
+btn.onclick = function(event) {
+    if(check == true) {
+        btn.style.position = 'absolute';
+        btn.style.zIndex = 1000;
+        moveAt(event);
+        document.onmousemove = moveAt;
+        check = false;
+    } else {
+        document.onmousemove = null;
+        check = true;
+    }
 }
-
 function moveAt(event) {
     btn.style.left = event.pageX - btn.offsetWidth / 2 +'px';
     btn.style.top = event.pageY - btn.offsetHeight / 2 +'px';
 }
-
-function move(event) {
-    moveAt(event);
-}
-
-function down() {
-    document.onmousemove = null;
-    btn.onclick = up;
-}
-
-//old code nested functions
-/*btn.onclick = function up(event) {
-    console.log("up");
-
-    btn.style.position = 'absolute';
-    btn.style.zIndex = 1000;
-    moveAt(event);
-
-    function moveAt(event) {
-        btn.style.left = event.pageX - btn.offsetWidth / 2 +'px';
-        btn.style.top = event.pageY - btn.offsetHeight / 2 +'px';
-    }
-
-    document.onmousemove = function (event) {
-        moveAt(event);
-    }
-
-    btn.onclick = function down() {
-        document.onmousemove = null;
-        console.log("down");
-        btn.onclick = up;
-    }
-
-}*/
